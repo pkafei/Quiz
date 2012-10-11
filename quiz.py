978f5f72ae1fd5d4fee69ed388d8cabedc37543d
Pkafei <plburton@gmail.com>
2012-10-10 22:26:10 -0700

    initial commit

---
+++ quiz.py
@@ -0,0 +1,98 @@
+#This is a quiz based on the one I took in class CS120
+
+print "This is a reveiw of Chapter 1. There are a total of 10 questions on the exam. 1-7 is multiple choice, and 8-10 is fill-in the blanks.\n\n "
+
+
+print "1. The display is often built into the system unit."
+print " a. Desktop \t b. Mobile_Devices \n c. Notebook \t d. Microcontroller "
+question_1 = raw_input(">")
+if question_1 == "b":
+	print "Correct!\n"
+else:
+	print "No, not quite.\n" 
+
+
+print "2. It performs the arithmetic, comparison, and other operations."
+print "a. Arithmetic_logic_unit\t b. RAM\n c. Control_Unit\t d. Keyboard"
+question_2 = raw_input(">")
+if question_2 == "a":
+	print "Nice one! \n"
+else:
+	print "That is not right. \n"
+
+
+print "3. This processor is usually used for servers, and low-end computer models."
+print "a. Core\t b. Itanium\n c. Pentium\t  d.Core"
+question_3 = raw_input(">")
+if question_3 == "b":
+	print "On fire :)\n"
+else:
+	print "Nope, it's not that.\n"
+
+
+print "4. Which memory type of memory is semipermanent, or permanent?"
+print "a.ROM\t b.RAM\n c.Flash\t d.CMOS"
+question_4 = raw_input(">")
+if question_4 == "a":
+	print "Just Mag :)\n"
+else:
+	print"Sorry Charlie, try again!\n"
+
+
+print "5.This computer chip maker is not American."
+print "a.Intel\t b.Celeron\n c.Radio_Shack_PC\t d.AMD"
+question_5 = raw_input(">")
+if question_5 == "d":
+	print "Awesome answer!\n"
+else:
+	print"Them are the breaks :(\n"
+
+
+print "6.This is part of the machine cycle that obtains program instructions or data from memory."
+print "a.Executing\t b.Decoding\n c.Fetching\t d.Storing"
+question_6 = raw_input(">")
+if question_6 == "c":
+	print "Dui dui dui!!!\n"
+else:
+	print "Not what I was looking for...\n"
+
+
+print"7.Bytes are usually expressed in intervals of..."
+print "a.9\t b.8\n c.2\t d.3"
+question_7 = raw_input(">")
+if question_7 == "b":
+	print "Score one!\n"
+else:
+	print"Come, again?\n"
+
+
+print"8.Which type of memory does the computer use when it is turned off?"
+print "a.ROM\t b.RAM\n c.Flash\t d.CMOS"
+question_8 = raw_input(">")
+if question_8 == "d":
+	print"You go boy!\n"
+else:
+	print"It is ok to be wrong sometimes.\n"
+
+
+print "9. This is used to accomodate major Asian languages."
+print"a.Pipeline\t b.ASCII\n c.Braille\t d.Unicode"
+question_9 = raw_input(">")
+if question_9 == "d":
+	print "You are gold, really!\n"
+else:
+	print "There are no words to express my disapointment.\n"
+
+print "10. In which unit is memory access time measured in?"
+print"a.nanoseconds\t b.milliseconds\n c.seconds\t d.minutes"
+question_10 = raw_input(">")
+if question_10 == "a":
+	print "Right, an excellent finish indeed!\n"
+else:
+	print "If you don't succeeed the first time, try, try again!\n"
+
+print "The end! :)"
+
+
+
+	
